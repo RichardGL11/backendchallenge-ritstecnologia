fd38e1ecc050938cc128b080bb26ecc90b9e449a
RichardGL11 <richardgreghilammenda@hotmail.com>
2024-12-19 18:37:06 -0300

    validation tests

--- tests/Feature/Api/Order/CreateOrderTest.php
+++ tests/Feature/Api/Order/CreateOrderTest.php
@@ -33,3 +33,76 @@ it('should be able to create an order',function (){
    ]);
 
 });
+
+describe('validation tests', function (){
+
+    test('user_id::validations',function ($rule, $value) {
+
+        $product = Product::factory()->create();
+
+        $request =  postJson(route('order.store'),[
+            'user_id'    => $value,
+            'product_id' => $product->id,
+            'status'     => OrderStatus::Pendente->value,
+
+        ]);
+
+        $request->assertJsonValidationErrors(['user_id' => $rule]);
+
+
+
+
+    })->with([
+        'required' => ['The user id field is required',''],
+        'exits' =>    ['The selected user id is invalid.', 2]
+    ]);
+
+    test('product_id::validations',function ($rule, $value) {
+
+        $user = User::factory()->create([
+            'phone' => '1111111111'
+        ]);
+
+        $request =  postJson(route('order.store'),[
+            'user_id'    => $user->id,
+            'product_id' => $value,
+            'status'     => OrderStatus::Pendente->value,
+
+        ]);
+
+        $request->assertJsonValidationErrors(['product_id' => $rule]);
+
+
+
+
+    })->with([
+        'required' => ['The product id field is required',''],
+        'exits' =>    ['The selected product id is invalid.', 2]
+    ]);
+    test('status::validations',function ($rule, $value) {
+
+        $user = User::factory()->create([
+            'phone' => '1111111111'
+        ]);
+        $product = Product::factory()->create();
+
+
+        $request =  postJson(route('order.store'),[
+            'user_id'    => $user->id,
+            'product_id' => $product->id,
+            'status'     => $value,
+
+        ]);
+
+        $request->assertJsonValidationErrors(['status' => $rule]);
+
+
+
+
+    })->with([
+        'required' => ['The status field is required.',''],
+        'enum'   =>   ['The selected status is invalid.','qualquer']
+    ]);
+
+
+});
