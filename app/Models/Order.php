af590e0b351d5ce9a146e522f29a83646174751e
RichardGL11 <richardgreghilammenda@hotmail.com>
2024-12-19 15:15:48 -0300

    creating relationship between those models

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Order extends Model
 {
@@ -11,4 +13,14 @@ class Order extends Model
     use HasFactory;
 
     protected $guarded = [];
+
+    public function user():BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function products():HasMany
+    {
+        return $this->hasMany(Product::class);
+    }
 }
